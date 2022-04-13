ce752f1fe55811aaabb266554314721d61bf15b3
github-action-benchmark <github@users.noreply.github.com>
2022-04-13 21:26:03 +0000

    add Benchmark (go) benchmark result for 51db69be0951d1b3244e63e2817b04710f0d9945

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649884302397,
+  "lastUpdate": 1649885163446,
   "repoUrl": "https://github.com/posteris/database",
   "entries": {
     "Benchmark": [
@@ -112,6 +112,120 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkNew_with_migrations/mysql-successfull",
             "value": 0.01062,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gsdenys@gmail.com",
+            "name": "Denys G. Santos",
+            "username": "gsdenys"
+          },
+          "committer": {
+            "email": "gsdenys@gmail.com",
+            "name": "Denys G. Santos",
+            "username": "gsdenys"
+          },
+          "distinct": true,
+          "id": "51db69be0951d1b3244e63e2817b04710f0d9945",
+          "message": "create full CI/CD pipeline",
+          "timestamp": "2022-04-13T18:19:39-03:00",
+          "tree_id": "b99904de42256e015fac2c4a5afd1a1d291eb49f",
+          "url": "https://github.com/posteris/database/commit/51db69be0951d1b3244e63e2817b04710f0d9945"
+        },
+        "date": 1649885163002,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGetAllowedDB/all-databases",
+            "value": 0.000004,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_getDatabaseType/none",
+            "value": 0.0000038,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_getDatabaseType/postgres",
+            "value": 0.000004,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_getDatabaseType/sqlite",
+            "value": 0.0000047,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_getDatabaseType/mysql",
+            "value": 0.0000043,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkNew/unknown",
+            "value": 0.0000055,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkNew/postgres-successfull",
+            "value": 0.006486,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkNew/postgres-error",
+            "value": 0.006732,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkNew/sqlite-successfull",
+            "value": 0.0003335,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkNew/sqlite-error",
+            "value": 0.0000677,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkNew/mysql-successfull",
+            "value": 0.001314,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkNew/mysql-error",
+            "value": 0.0000479,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkNew_with_migrations/postgres-successfull",
+            "value": 0.04921,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkNew_with_migrations/sqlite-successfull",
+            "value": 0.002311,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkNew_with_migrations/mysql-successfull",
+            "value": 0.01091,
             "unit": "ns/op",
             "extra": "1000000000 times\n2 procs"
           }
